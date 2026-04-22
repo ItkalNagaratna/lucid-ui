58e9a46e34642c6f23c424d3ae9b3826374d7b7c
Itkal Nagaratna <itkalnagaratna@gmail.com>
2026-04-22 13:49:47 +0530

    debug: direct render UIKit to resolve blank page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
 import UIKit from './pages/UIKit'
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/ui-kit" element={<UIKit />} />
+        <Route path="/" element={<UIKit />} />
       </Routes>
     </HashRouter>
   )
